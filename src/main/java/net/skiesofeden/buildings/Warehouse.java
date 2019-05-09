be37b9f91aca9912117d35f69623a6f0e9cd6660
TheDerpGamer <garret2kool@gmail.com>
2019-05-09 08:09:22 -0700

    Update Warehouse.java

--- src/main/java/net/skiesofeden/buildings/Warehouse.java
+++ src/main/java/net/skiesofeden/buildings/Warehouse.java
@@ -1,5 +1,16 @@
 package net.skiesofeden.buildings;
 
-public class Warehouse extends Building {
+import org.bukkit.inventory.Inventory;
 
+public class Warehouse extends Building {
+	
+	public static Inventory inventory1;
+	public static Inventory inventory2;
+	public static Inventory inventory3;
+	public static Inventory inventory4;
+	public static Inventory inventory5;
+	public static Inventory inventory6;
+	public static Inventory inventory7;
+	public static Inventory inventory8;
+	public static Inventory inventory9;
 }
